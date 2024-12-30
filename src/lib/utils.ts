bdca6d8325fb2d1aed5a751acc378f5f42ce5b6b
Alessandro Chinchilla <ale030401@gmail.com>
2024-12-29 19:17:05 -0600

    feat(tasks): add utility to group tasks by status and mock task data

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { TaskProps } from "@types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export function groupTasksByStatus(
+  tasks: TaskProps[],
+): Record<string, TaskProps[]> {
+  return tasks.reduce<Record<string, TaskProps[]>>((acc, task) => {
+    acc[task.status] = acc[task.status] || [];
+    acc[task.status].push(task);
+    return acc;
+  }, {});
 }
