ee5d861257c2907b524ecb9deb85bbd5f294c150
Alessandro Chinchilla <ale030401@gmail.com>
2024-12-30 19:33:29 -0600

    feat(env): add environment variables for GraphQL API and update vite config to integrate them

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react(), svgr(), tsconfigPaths()],
+    define: {
+      __ENV__: env,
+    },
+  };
 });
