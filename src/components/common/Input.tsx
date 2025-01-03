c2ea6ae5c912fd3e2fa98318b2457c33d802b955
Alessandro Chinchilla <ale030401@gmail.com>
2025-01-02 21:58:10 -0600

    refactor(input): enhance Input component with customizable props and controlled value handling

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,24 +1,32 @@
-import { useState } from "react";
+// import { useState } from "react";
 import Search from "@icons/Search.svg?react";
 import Close from "@icons/Close.svg?react";
+import type { InputProps } from "@types";
 
-export default function Input() {
-  const [value, setValue] = useState("");
-
-  const handleClear = () => setValue("");
+export default function Input({
+  id = "search",
+  placeholder = "Search",
+  className = "",
+  value,
+  onChange,
+  icon = true,
+}: InputProps) {
+  const handleClear = () => onChange("");
 
   return (
     <div className="relative flex items-center gap-6">
-      <label htmlFor="search">
-        <Search />
-      </label>
+      {icon && (
+        <label htmlFor={id}>
+          <Search />
+        </label>
+      )}
       <input
         type="text"
-        id="search"
+        id={id}
         value={value}
-        onChange={(e) => setValue(e.target.value)}
-        className="bg-neutral-4 text-body-m text-neutral-2 focus:outline-none focus:ring-0"
-        placeholder="Search"
+        onChange={(e) => onChange(e.target.value)}
+        className={`bg-neutral-4 text-body-m text-neutral-2 focus:outline-none focus:ring-0 ${className}`}
+        placeholder={placeholder}
       />
       {value && (
         <button
