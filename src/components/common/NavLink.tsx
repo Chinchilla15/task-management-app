d2f2d10504d9c2aafbf149f8be0ba28252843133
Alessandro Chinchilla <ale030401@gmail.com>
2025-01-07 15:00:39 -0600

    refactor(sidebar): enhance navigation links with routing and dynamic selection
    - Add navlink component to move through routes
    - Add additional configuration to nav items for better navigation

--- src/components/common/NavLink.tsx
+++ src/components/common/NavLink.tsx
@@ -1,30 +1,38 @@
 import { cn } from "@/lib/utils";
 import type { NavLinkProps } from "@types";
+import { NavLink, useLocation } from "react-router";
 
-export default function SidebarNavLink({
-  item,
-  isSelected,
-  onSelect,
-}: NavLinkProps) {
+export default function SidebarNavLink({ item, isSelected }: NavLinkProps) {
+  const location = useLocation();
   const Icon = item.icon;
+
+  const handleClick = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    if (item.path === location.pathname) {
+      e.preventDefault();
+      return;
+    }
+  };
 
   return (
     <li className="relative py-4">
-      <button
-        className={cn(
-          "flex w-full items-center justify-start gap-4 transition-colors duration-200",
-          "text-neutral-2 hover:text-primary-4",
-          isSelected && "text-primary-4",
-        )}
-        onClick={() => onSelect(item.id)}
+      <NavLink
+        to={item.path}
+        onClick={handleClick}
+        className={({ isActive }) =>
+          cn(
+            "flex w-full items-center justify-start gap-4 transition-colors duration-200",
+            "text-neutral-2 hover:text-primary-4",
+            (isActive || isSelected) && "text-primary-4",
+          )
+        }
         aria-current={isSelected ? "page" : undefined}
       >
         <Icon className="h-[18px] w-[18px]" />
         <p className="text-body-m font-semibold">{item.label}</p>
-      </button>
+      </NavLink>
       {isSelected && (
         <>
-          <div className="bg-primary-4 absolute right-0 top-0 h-full w-1" />
+          <div className="absolute right-0 top-0 h-full w-1 bg-primary-4" />
           <div className="pointer-events-none absolute inset-0 bg-gradient-to-r from-transparent to-red-500 opacity-10" />
         </>
       )}
--- src/components/ui/MasterSidebar.tsx
+++ src/components/ui/MasterSidebar.tsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
 import Logo from "@icons/Logo.svg?react";
 import SidebarNavLink from "@components/common/NavLink";
 import { navItems } from "@/config/sidebarNavigation";
+import { useLocation } from "react-router";
 
 export default function MasterSidebar() {
-  const [selected, setSelected] = useState("dashboard");
+  const location = useLocation();
+
+  const getSelectedItem = (pathname: string) => {
+    return (
+      navItems.find((item) => item.path === pathname)?.id ||
+      navItems.find((item) => pathname.startsWith(item.path))?.id ||
+      "dashboard"
+    );
+  };
+
+  const selectedId = getSelectedItem(location.pathname);
 
   return (
     <aside
@@ -21,8 +31,8 @@ export default function MasterSidebar() {
               <SidebarNavLink
                 key={item.id}
                 item={item}
-                isSelected={selected === item.id}
-                onSelect={setSelected}
+                isSelected={selectedId === item.id}
+                onSelect={() => {}}
               />
             ))}
           </ul>
