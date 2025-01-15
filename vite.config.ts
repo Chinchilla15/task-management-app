40be3a180b9792d18f7d32e461eef1d1c6467b50
Alessandro Chinchilla <ale030401@gmail.com>
2025-01-15 16:54:21 -0600

    feat(tests): add Vitest setup and configure testing environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,23 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig, loadEnv, mergeConfig, UserConfig } from "vite";
+import { defineConfig as defineVitestConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    plugins: [react(), svgr(), tsconfigPaths()],
-    define: {
-      __ENV__: env,
-    },
-  };
+const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/testing/setup.ts",
+  },
 });
+
+const viteConfig: UserConfig = defineConfig({
+  plugins: [react(), svgr(), tsconfigPaths()],
+  define: {
+    __ENV__: loadEnv(process.env.NODE_ENV || "development", process.cwd(), ""),
+  },
+});
+
+export default mergeConfig(vitestConfig, viteConfig);
